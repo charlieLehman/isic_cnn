eac3a728aba4d4e97faedd079bd1574d68539234
Charlie Lehman <charlie.k.lehman@gatech.edu>
2016-09-13 00:10:01 -0400

    Can generate JSON file of metadata for all images on isic-archive

--- makeimageidlist.py
+++ makeimageidlist.py
@@ -2,15 +2,29 @@ import json
 import requests
 import subprocess
 from pprint import pprint
+from time import sleep
+from tqdm import tqdm
 
-img_id_url = 'https://isic-archive.com:443/api/v1/image'
-img_metadata_url = 'https://isic-archive.com:443/api/v1/image/'
-img_id_payload = {'limit':'2'}
-img_id_request = requests.get(img_id_url, params=img_id_payload)
-img_id_list = img_id_request.json()
+isic_url = 'https://isic-archive.com:443/api/v1/image'
+isic_payload = {'limit':'20000'}
+isic_request = requests.get(isic_url, params=isic_payload)
+img_id_list = isic_request.json()
+img_meta ={} 
+img_dataset = []
    
-for n in range(0, len(img_id_list)-1):
+for n in tqdm(range(0, len(img_id_list))):
     #print(img_id_list[n]["_id"])#Print all ID's from get request
-    img_metadata_request = requests.get(img_metadata_url+img_id_list[n]['_id'])
-    img_metadata_
-print(img_metadata_request.json())
+    img_meta_request = requests.get(isic_url+'/'+img_id_list[n]['_id'])
+    img_meta_list = img_meta_request.json()
+    img_meta['id'] = img_id_list[n]['_id']
+    img_meta['filename'] = img_meta_list['name']
+    img_meta['age'] = img_meta_list['meta']['clinical']['age']
+    img_meta['sex'] = img_meta_list['meta']['clinical']['sex']
+    img_meta['b_m'] = img_meta_list['meta']['clinical']['benign_malignant']
+    img_meta['diagnosis'] = img_meta_list['meta']['clinical']['diagnosis']
+    if img_meta not in img_dataset:
+        img_dataset.append(dict(img_meta))
+
+f = open('img_dataset.json', 'w')
+f.write(json.dumps(img_dataset))
+f.close()
