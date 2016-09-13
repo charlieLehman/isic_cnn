be9f72aa3fbec2439041dfde4c96f52fb6781548
Charlie Lehman <charlie.k.lehman@gatech.edu>
2016-09-12 21:48:02 -0400

    using python

--- makeimageidlist.py
+++ makeimageidlist.py
@@ -1,10 +1,16 @@
 import json
-
+import requests
+import subprocess
 from pprint import pprint
 
-with open('/home/charlie/Pictures/isic_archive/image_list.json') as data_file:
-    data = json.load(data_file)
-
-f = open('test','w')
-for n in range(0,len(data)):
-    f.write(data[n]['_id']+'\n')
+img_id_url = 'https://isic-archive.com:443/api/v1/image'
+img_metadata_url = 'https://isic-archive.com:443/api/v1/image/'
+img_id_payload = {'limit':'2'}
+img_id_request = requests.get(img_id_url, params=img_id_payload)
+img_id_list = img_id_request.json()
+   
+for n in range(0, len(img_id_list)-1):
+    #print(img_id_list[n]["_id"])#Print all ID's from get request
+    img_metadata_request = requests.get(img_metadata_url+img_id_list[n]['_id'])
+    img_metadata_
+print(img_metadata_request.json())
