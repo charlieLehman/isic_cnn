5bb7006a1106f0445f07a3f5b6638cf6553fbb00
Charlie Lehman <charlie.k.lehman@gmail.com>
2016-09-22 00:47:13 -0400

    Iterative resizer for ISIC dataset

--- resize.py
+++ resize.py
@@ -1,9 +1,40 @@
 import cv2
 import numpy as np
+import os.path as op
+import json
+from tqdm import tqdm as bar
+from collections import Counter
 
-img = cv2.imread('/matlab/ISIC_0000054.jpg')
+import utils as api
+from rgb2binary import rgb2binary
 
-height, width = img.shape[:2]
-res = cv2.resize(img,(2*width, 2*height), interpolation = cv2.INTER_CUBIC)
+dataset_path = api.state.get()
+dataset = json.load(open(op.join(dataset_path, 'dataset.json')))
+count_benign = 0 
+count_malignant = 0
 
-imshow(res)
+def imageTo32(image):
+    try:
+        res = cv2.resize(im,(32,32), interpolation = cv2.INTER_CUBIC)
+        return res
+    except cv2.error as e:
+        print(e)
+
+for n in bar(range(0,len(dataset))):
+    for x in ['benign', 'malignant']:
+        if dataset[n]['b_m']==x:
+            count_benign += 1
+            filename = op.join(dataset_path,'images',x, dataset[n]['filename'])
+            if op.isfile(filename + '.jpg'):
+                try:
+                    im = cv2.imread(filename + '.jpg')
+                    cv2.imwrite(filename + '_32.jpg', imageTo32(im))
+                except cv2.error as e:
+                    print(dataset[n]['id'])
+            else:
+                print(dataset[n]['id'])
+
+
+benign_path = op.join(dataset_path,'images/benign')
+malign_path = op.join(dataset_path,'images/malignant')
+
