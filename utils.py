c0e9e61dd579459cce08c4bf63ba9beb51c9792e
Charlie Lehman <charlie.k.lehman@gmail.com>
2016-09-27 21:12:14 -0400

    Reogranized classes and renamed functions to be more representative of purpose

--- utils.py
+++ utils.py
@@ -7,39 +7,11 @@ from multiprocessing import Process, Pool
 from tqdm import tqdm
 from termcolor import colored, cprint
 
-class state:
-    def set(directory):
-        f = open('.state', 'w')
-        state = {}
-        state['dir'] = directory
-        json.dump(state,f)
-        f.close()
-
-    def exists():
-        return os.path.isfile('.state')
-
-    def clean_up():
-        if os.path.isfile('.state'):
-            current_dataset = json.load(open('.state'))
-            if ui.ynQuery("Should I delete " + current_dataset['dir']):
-                shutil.rmtree(current_dataset['dir'])
-                os.remove('.state')
-
-    def get():
-        if state.exists():
-            current_dataset = json.load(open('.state'))
-            return current_dataset['dir']
-        else:
-            cprint("You need to run setup.py", "red")
-            
-class api:
-#ISIC-Archive API url
-    isic_url = 'https://isic-archive.com:443/api/v1/image'
-
+class workingDirectory:
 #User input to use default or custom path to store dataset
-    def input_path():
-        if state.exists():
-            current_dataset = json.load(open('.state'))
+    def init():
+        if workingDirectory.exists():
+            current_dataset = json.load(open('.workingDirectory'))
             cprint("Your data is in "+ current_dataset['dir']+"\n","blue")
             dataset_path = current_dataset['dir']
         elif ui.ynQuery("Set up default path?\n /tmp/isic_dataset/"):
@@ -64,6 +36,34 @@ class api:
         else:
             return dataset_path
 
+    def save(directory):
+        f = open('.workingDirectory', 'w')
+        workingDirectory = {}
+        workingDirectory['dir'] = directory
+        json.dump(workingDirectory,f)
+        f.close()
+
+    def exists():
+        return os.path.isfile('.workingDirectory')
+
+    def clean_up():
+        if os.path.isfile('.workingDirectory'):
+            current_dataset = json.load(open('.workingDirectory'))
+            if ui.ynQuery("Should I delete " + current_dataset['dir']):
+                shutil.rmtree(current_dataset['dir'])
+                os.remove('.workingDirectory')
+
+    def get():
+        if workingDirectory.exists():
+            current_dataset = json.load(open('.workingDirectory'))
+            return current_dataset['dir']
+        else:
+            cprint("You need to run setup.py", "red")
+            
+class isic_api:
+#ISIC-Archive API url
+    isic_url = 'https://isic-archive.com:443/api/v1/image'
+
 #Generates a JSON file for the dataset
     def get_id_list(directory):
 #Get user input for the number of entries to collect
@@ -79,7 +79,7 @@ class api:
 
 #Try to grab the list of IDs from ISIC
         try:
-            isic_request = requests.get(api.isic_url, params=isic_payload)
+            isic_request = requests.get(isic_api.isic_url, params=isic_payload)
         except requests.exceptions.RequestException as e:
             cprint('There seems to be an issue with connecting...\n', 'red')
             return "Error: {}".format(e)
@@ -88,12 +88,15 @@ class api:
         img_id_list = isic_request.json()
         img_meta = {}
         dataset = []
+        count_images = len(img_id_list)
+        img_meta['count_benign']=0
+        img_meta['count_malign']=0
 
-        cprint('Accessing isic-archive\'s API. This will take a while...', 'blue')
+        cprint('Accessing isic-archive\'s API to retreive dataset ID list. This will take a while...', 'blue')
 
 #Iteratively query the API for each ID
-        for n in tqdm(range(0, len(img_id_list))):
-            img_meta_request = requests.get(api.isic_url + '/' + img_id_list[n]['_id'])
+        for n in tqdm(range(0, count_images)):
+            img_meta_request = requests.get(isic_api.isic_url + '/' + img_id_list[n]['_id'])
             assert isinstance(img_meta_request, object)
             img_meta_list = img_meta_request.json()
             img_meta['id'] = img_id_list[n]['_id']
@@ -112,7 +115,8 @@ class api:
         f.write(json.dumps(dataset))
         f.close()
         cprint('Dataset is in ' + os.path.abspath(directory), 'blue')
-        state.set(directory)
+        workingDirectory.save(directory)
+
 
 #Get the images from ISIC archive
     def get_images(directory):
@@ -126,8 +130,8 @@ class api:
             with open(os.path.join(directory,'dataset.json')) as dataset:
                 data = json.load(dataset)
 
-            api.__save_images__(directory, image_size, 'benign', [x for x in data if x['b_m'] == 'benign'])
-            api.__save_images__(directory, image_size, 'malignant', [x for x in data if x['b_m'] == 'malignant'])
+            isic_api.__save_images__(directory, image_size, 'benign', [x for x in data if x['b_m'] == 'benign'])
+            isic_api.__save_images__(directory, image_size, 'malignant', [x for x in data if x['b_m'] == 'malignant'])
 
         except FileNotFoundError as e:
             cprint('There is nothing here..\n', 'red')
@@ -144,7 +148,7 @@ class api:
         for n in tqdm(range(0,len(id_set))):
             img_name = id_set[n]['filename']
             with open(os.path.join(imdir,img_name)+'.jpg','wb') as f:
-                isic_request = requests.get(api.isic_url+'/'+id_set[n]['id']+'/thumbnail', params=isic_payload)
+                isic_request = requests.get(isic_api.isic_url+'/'+id_set[n]['id']+'/thumbnail', params=isic_payload)
                 f.write(isic_request.content)
 
 class ui:
@@ -169,3 +173,14 @@ class ui:
             else:
                 cprint("Please respond with 'yes' or 'no' "
                        "(or 'y' or 'n').\n", "red")
+class dataset:                
+#Return count of entry
+    def how_many(value, field):
+        try:
+            with open(os.path.join(workingDirectory.get(),'dataset.json')) as dataset:
+                return len([x for x in json.load(dataset) if x[field] == value])
+
+        except FileNotFoundError as e:
+            cprint('There is nothing here..\n', 'red')
+            print(e)
+            sys.exit(1)
