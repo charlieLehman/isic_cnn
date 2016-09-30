eadd1b306d1d261fc4c938f1e6230474ba2498c2
Charlie Lehman <charlie.k.lehman@gmail.com>
2016-09-29 21:50:40 -0400

    Added function header

--- m_code/block_dct.m
+++ m_code/block_dct.m
@@ -1,11 +1,19 @@
+## usage: [output_image] = block_dct (input_image, block_size)
+## 
+## This takes the blocked Discrete Cosine Transform of the
+## input image. 
+##
+## input_image expects a directory
+## block_size expects an integer
+##
+## Example:
+##
+##  image_DCT_8 = block_dct ("/images/lena.jpg", 8);
+##  imshow (image_DCT_8);
+
 function [output_image] = block_dct(input_image, block_size)
-
-
-I = double(input_image);
-
-T = dctmtx(block_size);
-dct = @(block_struct) T * block_struct.data * T';
-output_image = blockproc(I,[block_size block_size],dct);
-
-
+  I = double(input_image);
+  T = dctmtx(block_size);
+  dct = @(block_struct) T * block_struct.data * T';
+  output_image = blockproc(I,[block_size block_size],dct);
 end
